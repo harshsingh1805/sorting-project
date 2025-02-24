ed172e0f59eda47653f8bd79022675a1347668a1
harshsingh1805 <harshvardhansingh.is22@bmsce.ac.in>
2025-02-24 20:00:05 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -2,20 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
 app.use(express.json());
 
 // Serve frontend files
 app.use(express.static(path.join(__dirname, '../')));
 
+
 app.post('/sort', (req, res) => {
     const { algorithm, dataset } = req.body;
     const sortingAlgorithms = require('./sorting');
 
+    const sortingAlgorithms = require('./sorting');
+
     if (sortingAlgorithms[algorithm]) {
+        res.json(sortingAlgorithms[algorithm](dataset));
         res.json(sortingAlgorithms[algorithm](dataset));
     } else {
         res.status(400).json({ error: "Invalid sorting algorithm" });
@@ -27,3 +34,8 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.send("Backend is running!");
+});
+
+
