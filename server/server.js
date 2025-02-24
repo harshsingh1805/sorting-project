2d0f9d0d41d2fd85fbb98f5d84472bfc4df13a0e
Harshvardhan Singh <harshvardhansingh.is22@bmsce.ac.in>
2025-02-24 19:39:18 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 const express = require('express');
-const sortingAlgorithms = require('./sorting');
-const app = express();
+const cors = require('cors');
+const path = require('path');
 
-app.use(express.static('public'));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 app.use(express.json());
+
+// Serve frontend files
+app.use(express.static(path.join(__dirname, '../')));
 
 app.post('/sort', (req, res) => {
     const { algorithm, dataset } = req.body;
+    const sortingAlgorithms = require('./sorting');
+
     if (sortingAlgorithms[algorithm]) {
-        const sortedSteps = sortingAlgorithms[algorithm](dataset);
-        res.json(sortedSteps);
+        res.json(sortingAlgorithms[algorithm](dataset));
     } else {
         res.status(400).json({ error: "Invalid sorting algorithm" });
     }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.get('/', (req, res) => {
+    res.send("Backend is running!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
